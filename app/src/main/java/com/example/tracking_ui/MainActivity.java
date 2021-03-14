e825977931829c650a6a995a85bd99db947edc7a
Josh <43157130+joshdefreitas@users.noreply.github.com>
2021-03-14 19:06:15 -0400

    removed action bar and changed orientation to landscape

--- app/src/main/java/com/example/tracking_ui/MainActivity.java
+++ app/src/main/java/com/example/tracking_ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tracking_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove action bar
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+        //change orientation ot landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
